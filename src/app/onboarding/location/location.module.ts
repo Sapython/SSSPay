69a923661ed94ea8a1c69dbf7cbe65195854a652
brahma <beherabrahmananada77@gmail.com>
2022-06-20 00:08:26 +0530

    location-form complete

--- src/app/onboarding/location/location.module.ts
+++ src/app/onboarding/location/location.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { LocationPageRoutingModule } from './location-routing.module';
 
 import { LocationPage } from './location.page';
+import { BaseComponentsModule } from 'src/app/base-components/base-components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LocationPageRoutingModule
+    LocationPageRoutingModule,
+    BaseComponentsModule,
+ReactiveFormsModule
   ],
   declarations: [LocationPage]
 })
