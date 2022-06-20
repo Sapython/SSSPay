7fce2beb700dc82aabe26b9b59095c3135d6e5cd
vishnusharma0078 <88197254+vishnusharma0078@users.noreply.github.com>
2022-06-20 13:25:13 +0530

    onboarding/ phone and dob section completed

--- src/app/onboarding/phone-and-dob/phone-and-dob.module.ts
+++ src/app/onboarding/phone-and-dob/phone-and-dob.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { PhoneAndDobPageRoutingModule } from './phone-and-dob-routing.module';
 
 import { PhoneAndDobPage } from './phone-and-dob.page';
+import { BaseComponentsModule } from 'src/app/base-components/base-components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PhoneAndDobPageRoutingModule
+    PhoneAndDobPageRoutingModule,
+    BaseComponentsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [PhoneAndDobPage]
 })
