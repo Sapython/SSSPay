aa57aa8acce359868f850d51b97587926b7b1aa3
Saptam <saptampro2003@gmail.com>
2022-07-12 21:52:08 +0530

    SSSpay bank list page updated

--- src/app/pages/aeps/bank-list-modal/bank-list-modal.component.ts
+++ src/app/pages/aeps/bank-list-modal/bank-list-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { ModalController } from '@ionic/angular';
+import Fuse from 'fuse.js';
 @Component({
   selector: 'app-bank-list-modal',
   templateUrl: './bank-list-modal.component.html',
@@ -7,10 +8,34 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BankListModalComponent implements OnInit {
   @Input() banks:any[]= [];
-  constructor() { }
-
-  ngOnInit() {}
+  banksPart:any[]=[];
+  constructor(private modalController:ModalController) { }
+  filteredBanks:any[]=[];
+  ngOnInit() {
+    this.banksPart = this.banks.slice(0, 20);
+  }
   submit(bank:any){
     console.log(bank);
+  }
+  search(event:any){
+    // console.log(event.target.value);
+    const options = { keys: ['name'] };
+    const fuse = new Fuse(this.banks, options);
+    this.filteredBanks = []
+    const results = fuse.search(event.target.value);
+    results.forEach(element => {
+      this.filteredBanks.push(element.item);
+    });
+  }
+  closeModal(){
+    this.modalController.dismiss();
+  }
+  loadData(event:any){
+    console.log(event);
+    this.banksPart.push(...this.banks.slice(this.banksPart.length, this.banksPart.length+20))
+    if (this.banksPart.length == this.banks.length){
+      event.target.disabled = true;
+    }
+    event.target.complete();
   }
 }
