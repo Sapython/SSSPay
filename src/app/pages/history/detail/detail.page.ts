1028a82edac74024a83180d7271f895fa2ab125f
Saptam <saptampro2003@gmail.com>
2022-11-30 09:26:26 +0530

    Finished app

--- src/app/pages/history/detail/detail.page.ts
+++ src/app/pages/history/detail/detail.page.ts
@@ -6,7 +6,6 @@ import { TransactionService } from 'src/app/services/transaction.service';
 import { AlertsAndNotificationsService } from 'src/app/services/uiService/alerts-and-notifications.service';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
-  
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.page.html',
@@ -17,17 +16,17 @@ export class DetailPage implements OnInit {
   amount: number = 0;
   command: string = '';
   transactionId: string = '';
-  transactionData:any;
+  transactionData: any;
   date: Date = new Date();
   transactionSubscription: Subscription = Subscription.EMPTY;
   retailerId: string = '';
   retailerName: string = '';
-  successData:any;
+  successData: any;
   constructor(
     private activatedRoute: ActivatedRoute,
     private alertify: AlertsAndNotificationsService,
     private transactionService: TransactionService,
-    private dataProvider:DataProvider
+    private dataProvider: DataProvider,
   ) {
     this.activatedRoute.queryParams.subscribe((params) => {
       console.log(params);
@@ -35,6 +34,7 @@ export class DetailPage implements OnInit {
         const data = JSON.parse(params.data);
         this.transactionId = data.id;
         if (data.id) {
+          this.transactionSubscription.unsubscribe();
           this.transactionSubscription = this.transactionService
             .getTransaction(data.id)
             .subscribe((data) => {
@@ -45,6 +45,34 @@ export class DetailPage implements OnInit {
               this.command = data.type;
               this.date = data.date.toDate();
               this.successData = data.successData;
+              alert('updated');
+              if (data.type == 'expressPayout') {
+                if (data.newPayoutStatus) {
+                  alert(data.newPayOutStatus.event);
+                  let pendingStatuses = [
+                    'payout.pending',
+                    'payout.queued',
+                    'payout.initiated',
+                  ];
+                  let successStatuses = ['payout.processed'];
+                  let failedStatuses = [
+                    'payout.failed',
+                    'payout.reversed',
+                    'payout.rejected',
+                  ];
+                  if (pendingStatuses.includes(data.newPayOutStatus.event)) {
+                    this.paymentStatus = 'pending';
+                  } else if (
+                    successStatuses.includes(data.newPayOutStatus.event)
+                  ) {
+                    this.paymentStatus = 'success';
+                  } else if (
+                    failedStatuses.includes(data.newPayOutStatus.event)
+                  ) {
+                    this.paymentStatus = 'error';
+                  }
+                }
+              }
             });
         }
       }
@@ -53,28 +81,69 @@ export class DetailPage implements OnInit {
       console.log('params', params);
       if (params.id) {
         this.transactionId = params.id;
+        this.transactionSubscription.unsubscribe();
         this.transactionSubscription = this.transactionService
           .getTransaction(params.id)
           .subscribe((data) => {
-            console.log(data);
+            console.log(JSON.parse(JSON.stringify(data)));
             this.paymentStatus = data.status;
             this.amount = data.amount;
             this.command = data.type;
             this.date = data.date.toDate();
             this.transactionData = data;
             this.successData = data.successData;
+            if (data.type == 'expressPayout') {
+              let event = '';
+              try {
+                event = data.newPayoutStatus['event'];
+              } catch (error) {
+                event = data.newPayoutStatus.event;
+              }
+              console.log(
+                data.newPayoutStatus,
+                event,
+                data.newPayoutStatus != undefined
+              );
+              if (data.newPayoutStatus != undefined) {
+                let pendingStatuses = [
+                  'payout.pending',
+                  'payout.queued',
+                  'payout.initiated',
+                ];
+                let successStatuses = ['payout.processed'];
+                let failedStatuses = [
+                  'payout.failed',
+                  'payout.reversed',
+                  'payout.rejected',
+                ];
+                if (pendingStatuses.includes(event)) {
+                  this.paymentStatus = 'pending';
+                } else if (successStatuses.includes(event)) {
+                  this.paymentStatus = 'success';
+                } else if (failedStatuses.includes(event)) {
+                  this.paymentStatus = 'error';
+                }
+              }
+            }
           });
       }
     });
   }
 
-  getPdf(){
-
+  print() {
+    
   }
+
+  getPdf() {}
 
   ngOnInit() {
     this.retailerId = this.dataProvider.userData.userId;
     this.retailerName = this.dataProvider.userData.displayName;
+    setTimeout(() => {
+      window.print();
+      print()
+      alert('Printed');
+    }, 3000);
   }
   copy(text: string) {
     console.log(text);
