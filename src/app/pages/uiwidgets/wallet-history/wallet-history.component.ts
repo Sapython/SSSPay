d192ea01c4d75d40e5fcadd230cf6ec30f3d3653
Saptam <saptampro2003@gmail.com>
2023-02-05 14:02:45 +0530

    Fixed wallet and commmissions

--- src/app/pages/uiwidgets/wallet-history/wallet-history.component.ts
+++ src/app/pages/uiwidgets/wallet-history/wallet-history.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Timestamp } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-wallet-history',
@@ -6,14 +7,26 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./wallet-history.component.scss'],
 })
 export class WalletHistoryComponent implements OnInit {
-  @Input() statement:string;
-  @Input() date:Date;
-  @Input() money:number;
+  @Input() narration:string;
+  @Input() transactionTime:Date;
+  @Input() amount:number;
   @Input() type:'credit'|'debit' = 'credit';
   @Input() service:string;
   @Input() balance:number;
+  @Input() transactionId:string;
+  @Input() actionType:'TDS'|'Commission'|'Transaction-Credit'|'Transaction-Debit'|'Transaction-Refund';
   constructor() { }
 
   ngOnInit() {}
 
+}
+type WalletNarration = {
+  amount: number;
+  narration: string;
+  service: string;
+  balance: number;
+  transactionType: 'credit' | 'debit';
+  transactionId: string;
+  transactionTime: Timestamp;
+  actionType: 'TDS' | 'Commission' | 'Transaction-Credit' | 'Transaction-Debit' | 'Transaction-Refund'|'Direct-Debit'|'Direct-Credit';
 }
